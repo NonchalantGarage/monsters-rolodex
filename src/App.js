30bea9202e7bbc2b24a9d20fd1eade6d48a46048
curtis <hong.curtis@gmail.com>
2022-05-12 22:48:47 -0700

    added key to map function

--- src/App.js
+++ src/App.js
@@ -7,17 +7,23 @@ class App extends Component {
   constructor() {
     super();
 
-    this.state = {monsters: [{name: 'Linda'},{name: 'Frank'},{name:"Jacky"}]
+    this.state = {
+      monsters: [{ name: "Linda" }, { name: "Frank" }, { name: "Jacky" }],
     };
   }
 
   render() {
-    return <div className="App">
-      {this.state.monsters.map((monster) =>{
-        return <h1>{monster.name}</h1>
-      })}
-    </div>;
-    ;
+    return (
+      <div className="App">
+        {this.state.monsters.map((monster) => {
+          return (
+            <div key={monster.id}>
+              <h1>{monster.name}</h1>
+            </div>
+          );
+        })}
+      </div>
+    );
   }
 }
 
